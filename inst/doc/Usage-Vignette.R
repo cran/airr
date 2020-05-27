d4465a8cb8501fbb686e978e4acc8b211dc31302
Jason Vander Heiden <jason.vanderheiden@gmail.com>
2020-05-27 04:30:18 +0000

    version 1.3.0

--- inst/doc/Usage-Vignette.R
+++ inst/doc/Usage-Vignette.R
@@ -1,4 +1,4 @@
-## ----read_rearrangement, message=FALSE-----------------------------------
+## ----read_rearrangement, message=FALSE----------------------------------------
 library(airr)
 
 example_data <- system.file("extdata", "rearrangement-example.tsv.gz", package="airr")
@@ -7,7 +7,7 @@ airr_rearrangement <- read_rearrangement(example_data)
 class(airr_rearrangement)
 head(airr_rearrangement)
 
-## ----write_rearrangement-------------------------------------------------
+## ----write_rearrangement------------------------------------------------------
 out_file <- file.path(tempdir(), "airr_out.tsv")
 write_rearrangement(airr_rearrangement, out_file)
 
